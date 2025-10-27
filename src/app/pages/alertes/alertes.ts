a29dd3981be9b28207ed28a046d7bdcd6a6ec122
Ramatouu <ramatouyayakonare@gmail.com>
2025-10-27 17:41:47 +0000

    feat(users-page): Création complète de la page de gestion des utilisateurs (HTML/CSS/TS) + Finalisation du login.

---
+++ src/app/pages/alertes/alertes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alertes',
+  imports: [],
+  templateUrl: './alertes.html',
+  styleUrl: './alertes.css',
+})
+export class Alertes {
+
+}
